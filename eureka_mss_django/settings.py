3f89a0c349601a0cb492feb72fe041b52861a0a9
Vitor Soller <vgsoller@gmail.com>
2023-09-18 13:14:31 -0300

    add postgres config

--- eureka_mss_django/settings.py
+++ eureka_mss_django/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'eureka_mss_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Postgres2023!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
