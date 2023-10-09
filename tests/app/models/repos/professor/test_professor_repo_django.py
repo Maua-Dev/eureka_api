04107d389b6785ded9228f1f71f8c729c99bb4f6
Vitor Soller <vgsoller@gmail.com>
2023-10-09 10:22:56 -0300

    add Django TestCase

--- tests/app/models/repos/professor/test_professor_repo_django.py
+++ tests/app/models/repos/professor/test_professor_repo_django.py
@@ -1,14 +1,13 @@
-import pytest
 from app.models.repos.professor.professor_repo_django import ProfessorRepoDjango
 from app.models.models import Professor
+from django.test import TestCase
 
 def load_db():
     Professor.objects.create(professor_id=0, name="Joao Branco", rf="ABC")
     Professor.objects.create(professor_id=2, name="Joao Branco 2" , rf="2BC")
 
 
-class TestProfessorRepoDjango:
-    @pytest.mark.django_db
+class TestProfessorRepoDjango(TestCase):
     def test_get_all_professors(self):
         load_db()
         repo = ProfessorRepoDjango()
--- tests/app/models/repos/professor/test_professor_repo_mock.py
+++ tests/app/models/repos/professor/test_professor_repo_mock.py
@@ -1,7 +1,7 @@
 from app.models.repos.professor.professor_repo_mock import ProfessorRepoMock
+from django.test import TestCase
 
-
-class TestProfessorRepoMock:
+class TestProfessorRepoMock(TestCase):
     def test_get_all_professors(self):
         repo = ProfessorRepoMock()
         professors = repo.get_all_professors()
