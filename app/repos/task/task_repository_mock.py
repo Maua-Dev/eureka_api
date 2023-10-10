21c3d59697cea65eb6a5d8705d631a2defc59d18
Vitor Soller <vgsoller@gmail.com>
2023-10-09 23:57:00 -0300

    refat date

--- app/repos/task/task_repository_mock.py
+++ app/repos/task/task_repository_mock.py
@@ -12,43 +12,43 @@ class TaskRepositoryMock(ITaskRepository):
             {
                 "task_id": 1,
                 "name": "Dados do trabalho",
-                "delivery_date": "15/05/2023",
+                "delivery_date": "2023-05-15",
                 "responsible": USER_ROLE.STUDENT.value
             },
             {
                 "task_id": 2,
                 "name": "Dados do trabalho",
-                "delivery_date": "22/05/2023",
+                "delivery_date": "2023-05-22",
                 "responsible": USER_ROLE.ADVISOR.value
             },
             {
                 "task_id": 3,
                 "name": "Dados do trabalho",
-                "delivery_date": "14/09/2023",
+                "delivery_date": "2023-09-14",
                 "responsible": USER_ROLE.RESPONSIBLE.value
             },
             {
                 "task_id": 4,
                 "name": "Pôster Técnico (PDF)",
-                "delivery_date": "01/10/2023",
+                "delivery_date": "2023-10-01",
                 "responsible": USER_ROLE.STUDENT.value
             },
             {
                 "task_id": 5,
                 "name": "Pôster Técnico (PDF)",
-                "delivery_date": "03/10/2023",
+                "delivery_date": "2023-10-03",
                 "responsible": USER_ROLE.ADVISOR.value
             },
             {
                 "task_id": 6,
                 "name": "Pôster Imagem",
-                "delivery_date": "17/09/2023",
+                "delivery_date": "2023-09-17",
                 "responsible": USER_ROLE.STUDENT.value
             },
             {
                 "task_id": 7,
                 "name": "Pôster Imagem",
-                "delivery_date": "20/09/2023",
+                "delivery_date": "2023-09-20",
                 "responsible": USER_ROLE.ADVISOR.value
             },
         ]
