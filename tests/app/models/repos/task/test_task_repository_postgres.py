706553bdbbc3fa86c15b942203f6d4f6ec4dc06c
Vitor Soller <vgsoller@gmail.com>
2023-10-10 00:26:58 -0300

    refat test postgres

--- tests/app/models/repos/task/test_task_repository_postgres.py
+++ tests/app/models/repos/task/test_task_repository_postgres.py
@@ -1,18 +1,23 @@
+import datetime
+
 from django.test import TestCase
 
 from app.models import Task
 
 
-def load_db():
-    Task.objects.create(task_id=0, title="Delivery 1", delivery_date="2023-05-15", responsible="STUDENT")
-    Task.objects.create(task_id=1, title="Delivery 1", delivery_date="2023-05-22", responsible="ADVISOR")
-    Task.objects.create(task_id=2, title="Delivery 1", delivery_date="2023-09-14", responsible="RESPONSIBLE")
-    Task.objects.create(task_id=3, title="Delivery 2", delivery_date="2023-10-01", responsible="STUDENT")
-    Task.objects.create(task_id=4, title="Delivery 2", delivery_date="2023-10-03", responsible="ADVISOR")
-
-
 class TestrRepositoryPostgres(TestCase):
+    def setUp(self):
+        Task.objects.create(task_id=0, title="Delivery 1", delivery_date="2023-05-15", responsible="STUDENT")
+        Task.objects.create(task_id=1, title="Delivery 1", delivery_date="2023-05-22", responsible="ADVISOR")
+        Task.objects.create(task_id=2, title="Delivery 1", delivery_date="2023-09-14", responsible="RESPONSIBLE")
+        Task.objects.create(task_id=3, title="Delivery 2", delivery_date="2023-10-01", responsible="STUDENT")
+        Task.objects.create(task_id=4, title="Delivery 2", delivery_date="2023-10-03", responsible="ADVISOR")
+
     def test_get_all_tasks(self):
-        load_db()
         tasks = Task.objects.all()
-        assert len(tasks) == len(Task.objects.all())
+        assert len(tasks) == 5
+        assert tasks[0].task_id == 0
+        assert tasks[0].title == "Delivery 1"
+        assert tasks[0].delivery_date == datetime.date(2023, 5, 15)
+        assert tasks[0].responsible == "STUDENT"
+
