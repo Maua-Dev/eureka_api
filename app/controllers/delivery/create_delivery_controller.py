2a7bd46be468606fb28fcf6cef864c69a54694b6
Vitor Soller <vgsoller@gmail.com>
2023-10-23 20:06:05 -0300

    add expecion

--- app/controllers/delivery/create_delivery_controller.py
+++ app/controllers/delivery/create_delivery_controller.py
@@ -2,7 +2,7 @@ import json
 
 from app.controllers.controller_interface import IController
 from app.helpers.errors.common_errors import MissingParameters
-from app.helpers.http.http_codes import Created
+from app.helpers.http.http_codes import Created, BadRequest, InternalServerError
 from app.helpers.http.http_models import HttpRequestModel
 from app.repos.delivery.delivery_repository_interface import IDeliveryRepository
 
@@ -14,14 +14,25 @@ class CreateDeliveryController(IController):
         self.repo = repo
 
     def __call__(self, request: HttpRequestModel):
-        self.error_handling(request)
-        response_data = self.business_logic(request)
 
-        return Created(
-            body=response_data,
-            message="The delivery was created successfully"
-        )
+        try:
+            self.error_handling(request)
+            response_data = self.business_logic(request)
 
+            return Created(
+                body=response_data,
+                message="The delivery was created successfully"
+            )
+
+        except MissingParameters as e:
+            return BadRequest(
+                message=str(e)
+            )
+
+        except Exception as e:
+            return InternalServerError(
+                message=str(e)
+            )
 
     def error_handling(self, request: HttpRequestModel):
         if request.method != "POST":
