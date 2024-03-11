09ed5829f1859aa67500b53c3f8f80625a1805f0
Vitor Soller <vgsoller@gmail.com>
2024-03-11 11:31:54 -0300

    fix(rds): fix to before with generated secret

--- iac/iac/rds_stack.py
+++ iac/iac/rds_stack.py
@@ -16,17 +16,6 @@ class RDSStack(Construct):
     def __init__(self, scope: Construct, vpc: ec2.Vpc) -> None:
         super().__init__(scope, "EurekaRDSStack")
 
-
-        # create random secret for RDS
-        self.secret = secretsmanager.Secret(
-            self,
-            "EurekaRDSInstanceSecret",
-            generate_secret_string=secretsmanager.SecretStringGenerator(
-                exclude_punctuation=True
-            ),
-        )
-
-
         self.rds = rds.DatabaseInstance(
             self,
             "RDS",
@@ -37,7 +26,7 @@ class RDSStack(Construct):
                 ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO
             ),
             vpc=vpc,
-            credentials=rds.Credentials.from_secret(self.secret),
+            credentials=rds.Credentials.from_generated_secret("postgres"),
             removal_policy=RemovalPolicy.DESTROY,
             database_name="eureka",
             publicly_accessible=True,
